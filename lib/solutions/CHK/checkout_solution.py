88066cf9c44b3dd75c15068e4ef4c1260478cf31
sbx_user1051 <sbx_user1051@169.254.154.141>
2022-03-02 20:41:28 +0000

    Wed Mar 02 20:41:28 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,12 +3,37 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
+class DiscountFactory:
+
+    def build():
+        pass
+
+
+class AProductDiscountFactory(DiscountFactory):
+    pass
+
 
 class TellerSystem:
+    def __init__(self, skus):
+        self.skus = skus
 
-    def __init__(self):
+        self.stock = {
+            'A': {
+                'price': 50,
+            },
+            'B': {
+                'price': 30,
+            },
+            'C': {
+                'price': 20,
+            },
+            'D': {
+                'price': 15,
+            },
+        }
 
 
 def checkout(skus):
-    raise NotImplementedError()
+    teller = TellerSystem(skus=skus)
+
 
