5d87bd48f994320c47af0b8b503310fa9afeceec
sbx_user1051 <sbx_user1051@169.254.154.141>
2022-03-02 20:18:08 +0000

    Wed Mar 02 20:18:08 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,14 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
+class OutOfRangeException(Exception):
+    pass
+
+class NegativeValuesException(Exception):
+    pass
+
+def validate_inputs():
+
 def compute(x, y):
-    raise NotImplementedError()
+    
+
+
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,16 +1,23 @@
 import pytest
 
-from solutions.SUM import sum_solution
+from solutions.SUM import sum_solution, OutOfRangeException, NegativeValuesException
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.compute(50, 50) == 100
+        assert sum_solution.compute(100, 100) == 200
 
     def test_should_not_accept_negative_numbers():
-        with pytest.raises(Exception):
+        with pytest.raises(NegativeValuesException):
             sum_solution.compute(-1, 2)
 
     def test_should_not_accept_out_of_range_input():
-        with py
+        with pytest.raises(OutOfRangeException):
+            sum_solution.compute(200, 300)
+
+    def test_should_not_allow_non_ints():
+        with pytest.raises()
+
 
