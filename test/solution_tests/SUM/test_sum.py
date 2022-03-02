c13ed3f33820614ffd91dc5998b39cfa2a91c97f
sbx_user1051 <sbx_user1051@169.254.154.141>
2022-03-02 20:15:08 +0000

    Wed Mar 02 20:15:08 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,16 @@
+import pytest
+
 from solutions.SUM import sum_solution
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_should_not_accept_negative_numbers():
+        with pytest.raises(Exception):
+            sum_solution.compute(-1, 2)
+
+    def test_should_not_accept_out_of_range_input():
+        with py
+
