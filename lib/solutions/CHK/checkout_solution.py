d9fb20266291cd51663c60eac883c2bbc34409fb
sbx_user1051 <sbx_user1051@169.254.154.141>
2022-03-02 20:38:28 +0000

    Wed Mar 02 20:38:28 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,13 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+
+class TellerSystem:
+
+    def __init__(self):
+
+
 def checkout(skus):
     raise NotImplementedError()
+
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -9,7 +9,25 @@ def test_checkout_solution_sums_with_discounts():
     assert checkout_solution.checkout(skus) == 130
 
     skus = 'AAAA'
-    assert checkout_solution.checkout(skus) == 130
+    assert checkout_solution.checkout(skus) == 200
 
-    
+    skus = 'AAAAA'
+    assert checkout_solution.checkout(skus) == 250
+
+    skus = 'AAABB'
+    expected_A_cost = 130
+    expected_B_cost= 45
+    assert checkout_solution.checkout(skus) == (expected_A_cost + expected_B_cost)
+
+    skus = 'AAAB'
+    expected_A_cost = 130
+    expected_B_cost= 30
+    assert checkout_solution.checkout(skus) == (expected_A_cost + expected_B_cost)
+
+
+
+
+
+
+
 
