8fcf57424a56f8339125749a2a9b2f73e0ec3f7e
sbx_user1051 <sbx_user1051@169.254.154.141>
2022-03-02 20:24:08 +0000

    Wed Mar 02 20:24:08 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -10,10 +10,22 @@ class InvalidTypeProvidedException(Exception):
     pass
 
 def validate_inputs(x, y):
-    if isinstance(x, int) and is
+    if not isinstance(x, int) and not isinstance(y, int):
+        raise InvalidTypeProvidedException('Invalid input detected')
+
+    if x < 0 and y < 0:
+        raise NegativeValuesException('Negative values are not allowed')
+
+    if 0 <= x <= 100 and 0 <= y <= 100:
+        return True
+    
+    raise OutOfRangeException('You can only enter numbers between 0 and 100')
+
 
 def compute(x, y):
-    
+    try:
+        validate_inputs
+
 
 
 
