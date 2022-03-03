35e8cf9fd27255a01db9e9ab7a9d48fef550221b
sbx_user1051 <sbx_user1051@169.254.60.117>
2022-03-03 15:22:25 +0000

    Thu Mar 03 15:22:25 UTC 2022

--- lib/solutions/CHK/utils/factories.py
+++ lib/solutions/CHK/utils/factories.py
@@ -1,5 +1,7 @@
 from copy import deepcopy
 
+from ..checkout_solution import DISCOUNT_LIST
+
 class ProductDiscountFactorFactory:
 
     def __init__(self, discounts_to_apply=[]):
@@ -11,5 +13,13 @@ class ProductDiscountFactorFactory:
         index = 0
 
         while len(clone_product_subset) > 0:
-            
+            # Keep trying the same discount until we can't apply it anymore
+            while index != len(self.discounts_to_apply):
+                discount = self.discounts_to_apply[index]
+                selected_discount_attributes = DISCOUNT_LIST[discount]
+                min = selected_discount_attributes['rules']['min']
+                max = selected_discount_attributes['rules']['max']
+                desired_chunk_size = 
+                loading_factor = selected_discount_attributes['loading_factor']
+
         pass
