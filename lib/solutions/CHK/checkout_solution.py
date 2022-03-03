d9c7463c171198586fa40c8a01bad1a7c68addd1
sbx_user1051 <sbx_user1051@169.254.60.117>
2022-03-03 15:16:25 +0000

    Thu Mar 03 15:16:25 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -58,28 +58,15 @@ class DiscountHandler():
         }
 
     def apply(self, product_name, **kwargs):
-        discounts = self.discount_factories[product_name].build(
-            
-        )
+        discounts = self.discount_factories[product_name].build()
 
 
 class InvalidInputException(Exception):
     pass
 
-
-class TellerSystem:
-    def __init__(self):
-        self.stock = {
-            "A": {"price": 50, "discount_loading_factor": AProductDiscountFactory()},
-            "B": {"price": 30, "discount_loading_factor": BProductDiscountFactory()},
-            "C": {"price": 20, "discount_loading_factor": AbstractDiscountFactory()},
-            "D": {"price": 15, "discount_loading_factor": AbstractDiscountFactory()},
-            "E": {"price": 40, "discount_loading_factor": EProductDiscountFactory()},
-        }
-
-    def load_in_skus(self, skus):
-
-        allowed_choices = ["".join(key) for key in self.stock.keys()]
+def checkout(skus):
+    try:
+        allowed_choices = ["".join(key) for key in PRODUCT_STOCK_PRICES.keys()]
 
         if not isinstance(skus, str) or not all(
             allowed_sku in allowed_choices for allowed_sku in skus
@@ -87,30 +74,13 @@ class TellerSystem:
             raise InvalidInputException("Invalid input detected")
 
         sort_skus = ["".join(group) for _, group in groupby(sorted(skus))]
-        self.skus = sort_skus
 
-    def calculate_price(self):
-        products_list = self.skus
-        total_cost = 0
-        final_price = 0
-        for product_subset in products_list:
-            product_name = product_subset[0]
-            product_price = self.stock[product_name]["price"]
-            discount_factory = self.stock[product_name]["discount_loading_factor"]
-            total_cost += discount_factory.build(
-                product_subset,
-                product_price=product_price,
-                stock_list=self.stock,
-                product_list=products_list,
-            )
-
-        return total_cost
-
-
-def checkout(skus):
-    try:
-        teller = TellerSystem()
-        teller.load_in_skus(skus=skus)
-        return teller.calculate_price()
+        for sku_subset in sort_skus:
+            product_name = sku_subset[0]
+            product_price = PRODUCT_STOCK_PRICES[product_name]
+            price_before_discount = len(sku_subset) * product_price
+            price_after_discount = price_before_discount
+            discounts = DiscountHandler().apply(product_name, sku_subset=sku_subset, skus_full_list=sort_skus)
     except InvalidInputException:
         return -1
+
