1258914c11841459450824ff2897b5a1aedab8ac
sbx_user1051 <sbx_user1051@169.254.136.117>
2022-03-04 21:29:22 +0000

    Fri Mar 04 21:29:22 UTC 2022

--- lib/solutions/CHK/loading_factors/discount_loading_factors.py
+++ lib/solutions/CHK/loading_factors/discount_loading_factors.py
@@ -199,39 +199,37 @@ def product_loading_factor_one_discount(skus, product_list, product, product_nam
 
     return price, skus
 
-def product_multi_deal_loading_factor(skus, product_list, product, product_name, rules):
+def product_multi_deal_loading_factor(skus, product_list, product, product_name, rule):
     number_of_products = skus.count(product_name)
     product_price = product[product_name]
     discount_list = []
     remainder_product_count = 0
 
-    for rule in rules:
-        product_discount_data_object = product_list[product_name][rule]
+    product_discount_data_object = product_list[product_name][rule]
 
-        discount_threshold = product_discount_data_object['discount_threshold']
+    discount_threshold = product_discount_data_object['discount_threshold']
 
-        while number_of_products > 0:
-            prioritise_first_discount = number_of_products % discount_threshold
-
-            breakpoint()
-            
-            if prioritise_first_discount == 0:
-                product_discount_data_object['count'] += 1
-                number_of_products -= discount_threshold
-            elif number_of_products > 0 and prioritise_first_discount == discount_threshold:
-                product_discount_data_object['count'] += 1
-                number_of_products -= discount_threshold
-            else:
-                number_of_products -= 1
+    while number_of_products > 0:
+        prioritise_first_discount = number_of_products % discount_threshold
+        
+        if prioritise_first_discount == 0:
+            product_discount_data_object['count'] += 1
+            number_of_products -= discount_threshold
+        elif number_of_products > 0 and prioritise_first_discount == discount_threshold:
+            product_discount_data_object['count'] += 1
+            number_of_products -= discount_threshold
+        else:
+            number_of_products -= 1
 
         breakpoint()
-        applied_discount = product_discount_data_object['count']
 
-        remainder_product_count += skus.count(product_name) - (applied_discount * discount_threshold) 
+    applied_discount = product_discount_data_object['count']
 
-        apply_discount = (applied_discount * product_price * discount_threshold) - (applied_discount * 10)
+    remainder_product_count += skus.count(product_name) - (applied_discount * discount_threshold) 
 
-        discount_list.append(apply_discount)
+    apply_discount = (applied_discount * product_price * discount_threshold) - (applied_discount * 10)
+
+    discount_list.append(apply_discount)
 
     breakpoint()
     price = 0
@@ -258,19 +256,20 @@ def apply_price_loading_factors(skus, product_discount_list, products):
     for item in PRODUCT_STOCK_PRICES.keys():
         try:
             discount_loading_factor = get_loading_factor(item)
-            rules = product_discount_list[item].keys()
-            breakpoint()
-            if item in ['K', 'A']:
-                price, updated_skus = discount_loading_factor(skus, product_discount_list, products, item, rules)
-            else:
-                price, updated_skus = discount_loading_factor(skus, product_discount_list, products)
-            skus = updated_skus
-            final_price += price
+            for rule in product_discount_list[item].keys():
+                breakpoint()
+                if item in ['K', 'A']:
+                    price, updated_skus = discount_loading_factor(skus, product_discount_list, products, item, rule)
+                else:
+                    price, updated_skus = discount_loading_factor(skus, product_discount_list, products)
+                skus = updated_skus
+                final_price += price
         except KeyError:
             product_price = products[item]
             product_quantity = skus.count(item)
             final_price += product_price * product_quantity
     return final_price
+
 
 
 
