37c2f6da0fe0707c2cc31ddacd8979f16e977bcc
sbx_user1051 <sbx_user1051@169.254.72.249>
2022-03-05 17:00:34 +0000

    Sat Mar 05 17:00:34 UTC 2022

--- lib/solutions/CHK/loading_factors/discount_loading_factors.py
+++ lib/solutions/CHK/loading_factors/discount_loading_factors.py
@@ -127,9 +127,10 @@ def group_discount_loading_factor(skus, product_list, product, product_name, rul
 
     group_skus = "".join(groups)
     divisible = len(skus[0:3]) / 3
-    while divisible.is_integer():
+    while divisible.is_integer() and len(skus) > 0:
         skus = group_skus[int(3 * divisible):]
-        break
+
+    breakpoint()
 
     remainder_cost = 0
 
@@ -139,7 +140,6 @@ def group_discount_loading_factor(skus, product_list, product, product_name, rul
             remainder_cost += (1 * product[remaining_product_name])
             skus = skus[1:]
 
-    breakpoint()
     price = apply_discount + remainder_cost
     return price, skus
 
@@ -181,4 +181,5 @@ def apply_price_loading_factors(skus, product_discount_list, products):
             product_quantity = skus.count(item)
             final_price += product_price * product_quantity
     return final_price
+
 
