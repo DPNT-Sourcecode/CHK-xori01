293274553d3ee6c4e543b206abcb0b6898e0decd
sbx_user1051 <sbx_user1051@169.254.72.249>
2022-03-05 12:15:32 +0000

    Sat Mar 05 12:15:32 UTC 2022

--- lib/solutions/CHK/loading_factors/discount_loading_factors.py
+++ lib/solutions/CHK/loading_factors/discount_loading_factors.py
@@ -126,17 +126,17 @@ def group_discount_loading_factor(skus, product_list, product, product_name, rul
     apply_discount = (applied_discount * product_price * discount_threshold) - (applied_discount * discount_to_apply)
 
     group_skus = "".join(groups)
-    skus = group_skus[3:] if len(group_skus) > 2 else skus
 
     remainder_cost = 0
 
-    if len(skus) > 0:
-        for index in range(remainder_product_count):
-            breakpoint()
-            remaining_product_name = skus[index]
+    if len(group_skus) > 0:
+        for _ in range(remainder_product_count):
+            remaining_product_name = group_skus[0]
             remainder_cost += (1 * product[remaining_product_name])
-            skus = group_skus[1:]
+            group_skus = group_skus[1:]
+            breakpoint()
         
+    skus = group_skus[3:] if len(group_skus) > 2 else skus
     breakpoint()
     price = apply_discount + remainder_cost
     return price, skus
@@ -176,4 +176,5 @@ def apply_price_loading_factors(skus, product_discount_list, products):
             product_quantity = skus.count(item)
             final_price += product_price * product_quantity
     return final_price
+
 
