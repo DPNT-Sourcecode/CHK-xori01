cf353f937638209ec0d3441b781da6ba6ecce6d5
sbx_user1051 <sbx_user1051@169.254.72.249>
2022-03-05 17:06:34 +0000

    Sat Mar 05 17:06:34 UTC 2022

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -229,7 +229,7 @@ def test_v_discounts(skus, expected):
     # ('TTSZ', 66),
     # ('SXTZ', 66),
     # ('SXTZY', 86),
-    # ('ZSZSZ', 87),
+    ('ZSZSZ', 87),
     ('ZSZSZX', 90),
 ])
 def test_group_discounts(skus, expected):
